54957a972121c0d808fb62c881b5000e413fdf5b
Thato <t.chitja369@gmail.com>
2025-08-25 14:01:51 +0200

    Add NavBar, Footer, and embed the Home, BlogList and Blog Post components into the router outlet

--- NastyBar/src/app/app.routes.ts
+++ NastyBar/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)},
+
+    // Blog (SSG)
+    {path: '', loadComponent: () => import('./blog/blog-list/blog-list.component').then(m => m.BlogListComponent)},
+    {path: '', loadComponent: () => import('./blog/blog-post/blog-post.component').then(m => m.BlogPostComponent)},
+    
+];
