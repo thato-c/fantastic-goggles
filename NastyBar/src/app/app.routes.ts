4df20a982d0f32a5b75d162e66c9362fcf7853bd
Thato <t.chitja369@gmail.com>
2025-08-26 01:38:11 +0200

    Update the Blog Post Component

--- NastyBar/src/app/app.routes.ts
+++ NastyBar/src/app/app.routes.ts
@@ -6,5 +6,5 @@ export const routes: Routes = [
 
     // Blog (SSG)
     {path: 'blog', loadComponent: () => import('./blog/blog-list/blog-list.component').then(m => m.BlogListComponent)},
-    {path: 'blog/:slug', loadComponent: () => import('./blog/blog-post/blog-post.component').then(m => m.BlogPostComponent)},
+    {path: 'blog/:id', loadComponent: () => import('./blog/blog-post/blog-post.component').then(m => m.BlogPostComponent)},
 ];
--- NastyBar/src/app/blog/blog-post/blog-post.component.ts
+++ NastyBar/src/app/blog/blog-post/blog-post.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BlogPost } from '../../models/blog.model';
+import { BlogService } from '../../services/blog.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blog-post',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.css'
 })
-export class BlogPostComponent {
+export class BlogPostComponent implements OnInit{
+  post: BlogPost | undefined;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private blogService: BlogService
+  ){}
 
+  ngOnInit(): void{
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.post = this.blogService.getPostById(id);
+  }
 }
